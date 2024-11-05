31523823dc76e0b08c096f3e82b9ca4bab701b24
delster1 <delsterone@gmail.com>
2024-11-04 23:19:08 -0600

    fix silly

--- init.lua
+++ init.lua
@@ -5,19 +5,47 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.mapleader = ","
 
+--THEMES <leader>rt
+tocal themes = { "catppuccin", "tokyonight", "gruvbox", "everforest" }
+local current_theme_index = 1  -- Start with the first theme
+
+-- Function to set a theme
+local function set_theme(theme)
+    local success, _ = pcall(vim.cmd, "colorscheme " .. theme)
+    if success then
+        print("Switched to theme: " .. theme)
+    else
+        print("Failed to load theme: " .. theme)
+    end
+end
+
+-- Function to rotate themes
+local function rotate_theme()
+    current_theme_index = (current_theme_index % #themes) + 1
+    set_theme(themes[current_theme_index])
+end
+
+-- Set initial theme
+set_theme(themes[current_theme_index])
+
+-- Keybinding to rotate themes
+vim.keymap.set("n", "<leader>rt", rotate_theme, { desc = "Rotate Themes" })
+
+-- Lazy.nvim setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
 
 require("lazy").setup("plugins")
+vim.cmd("colorscheme everforest")
